8474d73e2ec2ecced0d233a481b8426535769cf5
abraham <abraham@expertcollective.org>
2020-02-08 10:35:38 -0600

    change name to expert-collective

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'test-app'`, () => {
+  it(`should have as title 'expert-collective'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('test-app');
+    expect(app.title).toEqual('expert-collective');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('test-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('expert-collective app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-app';
+  title = 'Expert Collective';
   param = { value: 'world' };
 
   constructor(translate: TranslateService) {
