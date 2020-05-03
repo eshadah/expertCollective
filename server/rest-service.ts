68428cd5f46925287669f785ab0636cda2c3cc1e
abraham <abraham@expertcollective.org>
2020-05-03 15:13:31 -0500

    Create server for REST APIs

---
+++ server/rest-service.ts
@@ -0,0 +1,15 @@
+import * as express from "express";
+import * as path from "path";
+import * as bodyParser from "body-parser";
+import { AddressInfo } from "net";
+
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+
+const server = app.listen(8000, "localhost", () => {
+  const {address, port} = server.address() as AddressInfo;
+  console.log('Listening on %s %s', address, port);
+});
